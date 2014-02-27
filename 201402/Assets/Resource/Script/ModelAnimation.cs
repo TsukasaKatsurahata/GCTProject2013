b4b968fcaeda9eb4d8d59921604570e7a2c70a53
TsukasaKatsurahata <s1123157@cce.kanagawa-it.ac.jp>
2014-02-27 09:31:47 +0900

    Hornet AI 完成（仮）
    未完成の部分

---
+++ 201402/Assets/Resource/Script/ModelAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelAnimation : MonoBehaviour {
+	private Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim == null) return;
+		if (!anim.isPlaying) anim.Play("Take 001");
+	}
+
+	public void SetAnimSpeer() {
+		if (anim == null) return;
+		anim.Play("Take 002");
+	}
+
+	public void SetAnimDead() {
+		if (anim == null) return;
+		anim.Play("Take 003");
+	}
+}
