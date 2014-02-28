3a0dc5cf60af1fcc4bd39f68c68b80671d876a3a
TsukasaKatsurahata <s1123157@cce.kanagawa-it.ac.jp>
2014-02-28 14:18:50 +0900

    Debug　Oculus　Scene
    Oculusのシーン遷移間でデータが保管されているかをテスト
    Uniduinoの引継ぎにバグが無いか検証
    Titleシーンで蜂の巣の前まで移動する

---
+++ 201402/Assets/Resource/Script/TitleToStart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleToStart : MonoBehaviour {
+	public Animation StartAnim = null;
+	private float Timer;
+	public string NextScene = "mainGame";
+
+	// Use this for initialization
+	void Start () {
+		if (StartAnim == null) StartAnim = GetComponent<Animation>();
+		Timer = -1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Timer > 0.0f) {
+			if (Time.time > Timer) {
+				Application.LoadLevel(NextScene);
+			}
+		}
+		else {
+			if (ArduinoInput.InputData > 0) {
+				if (StartAnim != null) StartAnim.Play(StartAnim.clip.name);
+				Timer = Time.time + 16.0f;
+			}
+		}
+	}
+}
---
+++ 201402/Assets/Resource/UniduinoScript/ArduinoDisconnect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uniduino;
+
+public class ArduinoDisconnect : MonoBehaviour {
+	public Arduino arduino;
+
+	// Use this for initialization
+	void Start () {
+		arduino = Arduino.global;
+	}
+
+	void OnDestroy() {
+		arduino.Disconnect();
+	}
+}
